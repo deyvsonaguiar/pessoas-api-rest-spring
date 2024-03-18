f5f2a31a96a60aac007e84c052c03ceabe49c340
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2024-03-18 10:58:26 -0400

    Criação e injeção do PessoaGateway no CreatePessoaUseCaseImplementation

---
+++ src/main/java/com/deyvsonaguiar/pessoasapi/core/gateways/PessoaGateway.java
@@ -0,0 +1,8 @@
+package com.deyvsonaguiar.pessoasapi.core.gateways;
+
+import com.deyvsonaguiar.pessoasapi.core.entities.Pessoa;
+
+public interface PessoaGateway {
+
+    Pessoa createPessoa(Pessoa pessoa);
+}
--- src/main/java/com/deyvsonaguiar/pessoasapi/core/useCases/CreatePessoaUseCaseImplementation.java
+++ src/main/java/com/deyvsonaguiar/pessoasapi/core/useCases/CreatePessoaUseCaseImplementation.java
@@ -1,10 +1,18 @@
 package com.deyvsonaguiar.pessoasapi.core.useCases;
 
 import com.deyvsonaguiar.pessoasapi.core.entities.Pessoa;
+import com.deyvsonaguiar.pessoasapi.core.gateways.PessoaGateway;
 
 public class CreatePessoaUseCaseImplementation implements CreatePessoaUseCase {
+
+    private final PessoaGateway pessoaGateway;
+
+    public CreatePessoaUseCaseImplementation(PessoaGateway pessoaGateway) {
+        this.pessoaGateway = pessoaGateway;
+    }
+
     @Override
     public Pessoa execute(Pessoa pessoa) {
-        return null;
+        return pessoaGateway.createPessoa(pessoa);
     }
 }
